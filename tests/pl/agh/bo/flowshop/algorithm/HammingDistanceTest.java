e5b82c66c675abd621b435f0cc583513b8baebec
fanfilmu <fanflm@gmail.com>
2015-04-21 00:59:20 +0200

    General fix

--- tests/pl/agh/bo/flowshop/algorithm/HammingDistanceTest.java
+++ tests/pl/agh/bo/flowshop/algorithm/HammingDistanceTest.java
@@ -1,6 +1,7 @@
 package pl.agh.bo.flowshop.algorithm;
 
 import org.junit.Test;
+import pl.agh.bo.flowshop.Job;
 
 import static org.junit.Assert.*;
 
@@ -8,18 +9,22 @@ public class HammingDistanceTest {
 
     @Test
     public void testCalculateDistance() throws Exception {
-        int[][] sets = getSets();
+        Job[][] sets = getSets();
 
         assertEquals(5, HammingDistance.calculateDistance(sets[0], sets[1]).intValue());
         assertEquals(5, HammingDistance.calculateDistance(sets[0], sets[2]).intValue());
         assertEquals(6, HammingDistance.calculateDistance(sets[1], sets[2]).intValue());
     }
 
-    private int[][] getSets() {
-        int[][] result = new int[8][2];
-        result[0] = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
-        result[1] = new int[] { 0, 7, 2, 5, 3, 4, 6, 1 };
-        result[2] = new int[] { 1, 7, 2, 3, 4, 6, 5, 0 };
+    private Job[][] getSets() {
+        Job[] jobs = new Job[8];
+        for (int i = 0; i < 8; i++)
+            jobs[i] = new Job(i, new Integer[] { 1, 2, 3 });
+        
+        Job[][] result = new Job[8][2];
+        result[0] = new Job[] { jobs[0], jobs[1], jobs[2], jobs[3], jobs[4], jobs[5], jobs[6], jobs[7] };
+        result[1] = new Job[] { jobs[0], jobs[7], jobs[2], jobs[5], jobs[3], jobs[4], jobs[6], jobs[1] };
+        result[2] = new Job[] { jobs[1], jobs[7], jobs[2], jobs[3], jobs[4], jobs[6], jobs[5], jobs[0] };
 
         return result;
     }
