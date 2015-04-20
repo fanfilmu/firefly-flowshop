8ccc4cdf0c7adb15838dbeba24254f8dc5212e00
Andrzej_Ryl <Andrzej.Ryl@gmail.com>
2015-04-21 00:31:16 +0200

    Sceleton for algorithm code

--- src/pl/agh/bo/flowshop/algorithm/Algorithm.java
+++ src/pl/agh/bo/flowshop/algorithm/Algorithm.java
@@ -1,9 +1,19 @@
 package pl.agh.bo.flowshop.algorithm;
 
+import pl.agh.bo.flowshop.Firefly;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Andrzej on 2015-04-21.
  */
 public class Algorithm {
+
+    private final static long MAX_ITERATIONS = 2000;
+
+    private Map<Firefly, Long> fireflies;
+
 
     /**
      * Pseudcode
@@ -19,4 +29,50 @@ public class Algorithm {
      * 4. Recalculate light intensity for every firefly
      * 5. Move randomly the best firefly ?? (that's actually not ideal right now)
      */
+
+    public void start(long initialSeed) {
+        int i = 0;
+        Firefly bestOne;
+
+        // Generate fireflies based on initial seed (calculate their light intensity as well)
+        fireflies = generateFireflies(initialSeed);
+
+        while (i++ < MAX_ITERATIONS) {
+            for (Firefly fireflyA : fireflies.keySet()) {
+                for (Firefly fireflyB : fireflies.keySet()) {
+                    if (!fireflyA.equals(fireflyB)) {
+                        // Check light intensity and move fireflies
+                        // using PMX crossing
+                        // TODO: Use PMX here to move fireflies
+                    }
+                }
+            }
+
+            // Recalculate light intensity for every firefly
+            fireflies = recalculateIntensity(fireflies);
+
+            // Find the best firefly
+            bestOne = findBest(fireflies);
+
+            bestOne = moveRandomly(bestOne);
+        }
+
+
+    }
+
+    private Firefly moveRandomly(Firefly bestOne) {
+        return null;
+    }
+
+    private Firefly findBest(Map<Firefly, Long> fireflies) {
+        return null;
+    }
+
+    private Map<Firefly, Long> recalculateIntensity(Map<Firefly, Long> fireflies) {
+        return null;
+    }
+
+    private Map<Firefly, Long> generateFireflies(long initialSeed) {
+        return null;
+    }
 }
