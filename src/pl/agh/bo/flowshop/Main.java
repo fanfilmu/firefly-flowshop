3d44be81f2c981775181b3c31bce3c137937b504
fanfilmu <fanflm@gmail.com>
2015-04-20 20:14:47 +0200

    Add placeholder Main class

---
+++ src/pl/agh/bo/flowshop/Main.java
@@ -0,0 +1,10 @@
+package pl.agh.bo.flowshop;
+
+/**
+ * Created by fan on 20.04.15.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Dobry.");
+    }
+}
