998d54ec0f912ad9d49cbc5c40c28656d149e9ee
Andrzej_Ryl <Andrzej.Ryl@gmail.com>
2015-05-04 21:16:00 +0200

    Add data connection
    Added connections between view and fields in Main class

--- src/pl/agh/bo/flowshop/Main.java
+++ src/pl/agh/bo/flowshop/Main.java
@@ -5,6 +5,8 @@ import pl.agh.bo.flowshop.solver.Solver;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
@@ -13,7 +15,7 @@ import java.util.List;
 /**
  * Created by fan on 20.04.15.
  */
-public class Main {
+public class Main extends Component {
     private JLabel titleLabel;
     private JPanel mainPanel;
     private JPanel leftPanel;
@@ -29,9 +31,60 @@ public class Main {
     private JCheckBox CDSCheckBox;
     private JRadioButton PMXRadioButton;
     private JRadioButton swapRadioButton;
+    private String maxIterations;
+    private String populationSize;
+    private String absorbtionCoefficient;
+    private String baseAttraction;
+    private String lightAbsorption;
+    private boolean usesNEH;
+    private boolean usesCDS;
+    private String path = "";
+    private String crossoverStrategy = "PMX";
+
+    public Main() {
+        chooseFileWithDataButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Open file chooser form
+                final JFileChooser fc = new JFileChooser();
+                int returnVal = fc.showOpenDialog(Main.this);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    // Assign path to a file chosen by user
+                    path = fc.getSelectedFile().getAbsolutePath();
+                }
+            }
+        });
+        startAlgorithmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Start algorithm
+
+                // Gather data from fields
+                getData(Main.this);
+
+                System.out.println("iter: " + maxIterations + ", population: " + populationSize + ", absorb: " + absorbtionCoefficient
+                 + ", base: " + baseAttraction + ", light: " + lightAbsorption + ", neh: " + usesNEH + ", cds: " + usesCDS
+                + ", strategy: " + crossoverStrategy + ", path: " + path );
+            }
+        });
+
+        PMXRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                crossoverStrategy = "PMX";
+            }
+        });
+        swapRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                crossoverStrategy = "swap";
+            }
+        });
+    }
 
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
 
+        Main main = new Main();
         // Set cross-platform Java L&F (also called "Metal")
         UIManager.setLookAndFeel(
                 UIManager.getSystemLookAndFeelClassName());
@@ -40,6 +93,82 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public String getMaxIterations() {
+        return maxIterations;
+    }
+
+    public void setMaxIterations(final String maxIterations) {
+        this.maxIterations = maxIterations;
+    }
+
+    public String getPopulationSize() {
+        return populationSize;
+    }
+
+    public void setPopulationSize(final String populationSize) {
+        this.populationSize = populationSize;
+    }
+
+    public String getAbsorbtionCoefficient() {
+        return absorbtionCoefficient;
+    }
+
+    public void setAbsorbtionCoefficient(final String absorbtionCoefficient) {
+        this.absorbtionCoefficient = absorbtionCoefficient;
+    }
+
+    public String getBaseAttraction() {
+        return baseAttraction;
+    }
+
+    public void setBaseAttraction(final String baseAttraction) {
+        this.baseAttraction = baseAttraction;
+    }
+
+    public String getLightAbsorption() {
+        return lightAbsorption;
+    }
+
+    public void setLightAbsorption(final String lightAbsorption) {
+        this.lightAbsorption = lightAbsorption;
+    }
+
+    public boolean isUsesNEH() {
+        return usesNEH;
+    }
+
+    public void setUsesNEH(final boolean usesNEH) {
+        this.usesNEH = usesNEH;
+    }
+
+    public boolean isUsesCDS() {
+        return usesCDS;
+    }
+
+    public void setUsesCDS(final boolean usesCDS) {
+        this.usesCDS = usesCDS;
+    }
+
+    public void setData(Main data) {
+        maxIterationsTV.setText(data.getMaxIterations());
+        populationSizeTV.setText(data.getPopulationSize());
+        absorbtionCoefficientTV.setText(data.getAbsorbtionCoefficient());
+        baseAttractionTV.setText(data.getBaseAttraction());
+        lightAbsorptionTV.setText(data.getLightAbsorption());
+        NEHCheckBox.setSelected(data.isUsesNEH());
+        CDSCheckBox.setSelected(data.isUsesCDS());
+    }
+
+    public void getData(Main data) {
+        data.setMaxIterations(maxIterationsTV.getText());
+        data.setPopulationSize(populationSizeTV.getText());
+        data.setAbsorbtionCoefficient(absorbtionCoefficientTV.getText());
+        data.setBaseAttraction(baseAttractionTV.getText());
+        data.setLightAbsorption(lightAbsorptionTV.getText());
+        data.setUsesNEH(NEHCheckBox.isSelected());
+        data.setUsesCDS(CDSCheckBox.isSelected());
     }
     /*
     public static void main(String args[]) {
